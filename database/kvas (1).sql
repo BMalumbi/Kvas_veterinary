33d081d9a3cbb26bc2fda385b8f653479c3c87d0
Benoit BEYA <benoitbeya69@gmail.com>
2025-03-31 12:55:54 +0200

    Reactualisation du projet

--- database/kvas (1).sql
+++ database/kvas (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 04, 2022 at 10:36 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 31 mars 2025 à 10:38
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,59 +18,70 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kvas_db`
+-- Base de données : `kvas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointment_list`
+-- Structure de la table `appointment_list`
 --
 
-CREATE TABLE `appointment_list` (
-  `id` int(30) NOT NULL,
+DROP TABLE IF EXISTS `appointment_list`;
+CREATE TABLE IF NOT EXISTS `appointment_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `code` varchar(100) NOT NULL,
   `schedule` date NOT NULL,
   `owner_name` text NOT NULL,
   `contact` text NOT NULL,
   `email` text NOT NULL,
   `address` text NOT NULL,
-  `category_id` int(30) NOT NULL,
+  `category_id` int NOT NULL,
   `breed` text NOT NULL,
   `age` varchar(50) NOT NULL,
-  `service_ids` text DEFAULT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Color` varchar(100) NOT NULL,
+  `service_ids` text,
+  `status` tinyint NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `appointment_list`
+-- Déchargement des données de la table `appointment_list`
 --
 
-INSERT INTO `appointment_list` (`id`, `code`, `schedule`, `owner_name`, `contact`, `email`, `address`, `category_id`, `breed`, `age`, `service_ids`, `status`, `date_created`, `date_updated`) VALUES
-(1, 'KVAS-2022010001', '2025-01-04', 'John D Smith', '0912345789', 'jsmith@sample.com', 'Sun City', 1, 'German Shepherd', '5 mos. old', '3,1', 0, '2025-01-04 15:55:21', NULL),
-(2, 'KVAS-2022010002', '2025-01-05', 'Deoel M', '09123456897', 'mdeoel@sample.com', 'Lshi City', 1, 'Pug', '6 mos. old', '3,1', 0, '2025-01-04 15:56:55', NULL),
-(3, 'KVAS-2022010003', '2025-01-05', 'Akash B.', '094567894152', 'bakash@gmail.com', 'Kzi', 2, 'Persian Cat', '1 yr. old', '5,3', 0, '2025-01-04 15:58:23', NULL),
-(4, 'KVAS-2022010004', '2025-01-05', 'Wonder Tshi', '0995564887', 'twonder@sample.com', 'Leopold City', 2, 'Maine Coon', '7 mos.', '5,1', 0, '2025-01-04 15:59:51', NULL),
-(6, 'KVAS-2022010006', '2025-01-06', 'Golden Jax', '09888754466', 'jgolden@gmail.com', 'Jado City', 2, 'Ragdoll', '8 mos', '5,1', 1, '2025-01-04 16:27:54', '2025-01-04 17:17:55');
+INSERT INTO `appointment_list` (`id`, `code`, `schedule`, `owner_name`, `contact`, `email`, `address`, `category_id`, `breed`, `age`, `Color`, `service_ids`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'KVAS-2022010001', '2025-01-04', 'John D Smith', '0912345789', 'jsmith@sample.com', 'Sun City', 1, 'German Shepherd', '5 mos. old', '', '3,1', 0, '2025-01-04 15:55:21', NULL),
+(2, 'KVAS-2022010002', '2025-01-05', 'Deoel M', '09123456897', 'mdeoel@sample.com', 'Lshi City', 1, 'Pug', '6 mos. old', '', '3,1', 0, '2025-01-04 15:56:55', NULL),
+(3, 'KVAS-2022010003', '2025-01-05', 'Akash B.', '094567894152', 'bakash@gmail.com', 'Kzi', 2, 'Persian Cat', '1 yr. old', '', '5,3', 0, '2025-01-04 15:58:23', NULL),
+(4, 'KVAS-2022010004', '2025-01-05', 'Wonder Tshi', '0995564887', 'twonder@sample.com', 'Leopold City', 2, 'Maine Coon', '7 mos.', '', '5,1', 0, '2025-01-04 15:59:51', NULL),
+(6, 'KVAS-2022010006', '2025-01-06', 'Golden Jax', '09888754466', 'jgolden@gmail.com', 'Jado City', 2, 'Ragdoll', '8 mos', '', '5,1', 1, '2025-01-04 16:27:54', '2025-01-04 17:17:55'),
+(8, 'KVAS-2025030001', '2025-03-31', 'benoit', '0990794163', 'benbeyamalumbi@gmail.com', 'brasimba', 1, 'berger allemand', '2', '', '2', 1, '2025-03-31 09:13:00', '2025-03-31 09:14:47'),
+(9, 'KVAS-2025030002', '2025-04-01', 'jeslone', '0987645432', 'jeslonelukisa@gmail.com', 'dispensaire', 2, 'chat noir', '2', '', '2', 1, '2025-03-31 10:05:42', '2025-03-31 10:07:47'),
+(10, 'KVAS-2025030003', '2025-04-01', 'jeslone', '0987645432', 'jeslonelukisa@gmail.com', 'vermoise', 2, 'chat noir', '2', 'jaune', '3', 0, '2025-03-31 10:26:43', NULL),
+(11, 'KVAS-2025030004', '2025-04-01', 'jeslone', '0987645432', 'jeslonelukisa@gmail.com', 'vermoise', 2, 'chat noir', '2', 'jaune', '2', 0, '2025-03-31 10:55:54', NULL),
+(12, 'KVAS-2025030005', '2025-04-01', 'jeslone', '0987645432', 'jeslonelukisa@gmail.com', '', 2, 'chat noir', '2', 'rouge', '3', 0, '2025-03-31 11:03:17', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category_list`
+-- Structure de la table `category_list`
 --
 
-CREATE TABLE `category_list` (
-  `id` int(30) NOT NULL,
+DROP TABLE IF EXISTS `category_list`;
+CREATE TABLE IF NOT EXISTS `category_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = Active, 1 = Delete',
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = Active, 1 = Delete',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `category_list`
+-- Déchargement des données de la table `category_list`
 --
 
 INSERT INTO `category_list` (`id`, `name`, `delete_flag`, `date_created`, `date_updated`) VALUES
@@ -79,26 +90,29 @@ INSERT INTO `category_list` (`id`, `name`, `delete_flag`, `date_created`, `date_
 (3, 'Hamsters', 0, '2025-01-04 10:32:02', NULL),
 (4, 'Rabbits', 0, '2025-01-04 10:32:13', NULL),
 (5, 'Birds', 0, '2025-01-04 10:32:47', NULL),
-(6, 'test', 1, '2025-01-04 10:33:02', NULL);
+(6, 'test', 1, '2025-01-04 10:33:02', NULL),
+(7, 'Oiseaux', 0, '2025-03-31 09:20:51', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `message_list`
+-- Structure de la table `message_list`
 --
 
-CREATE TABLE `message_list` (
-  `id` int(30) NOT NULL,
+DROP TABLE IF EXISTS `message_list`;
+CREATE TABLE IF NOT EXISTS `message_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `fullname` text NOT NULL,
   `contact` text NOT NULL,
   `email` text NOT NULL,
   `message` text NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `message_list`
+-- Déchargement des données de la table `message_list`
 --
 
 INSERT INTO `message_list` (`id`, `fullname`, `contact`, `email`, `message`, `status`, `date_created`) VALUES
@@ -107,22 +121,24 @@ INSERT INTO `message_list` (`id`, `fullname`, `contact`, `email`, `message`, `st
 -- --------------------------------------------------------
 
 --
--- Table structure for table `service_list`
+-- Structure de la table `service_list`
 --
 
-CREATE TABLE `service_list` (
-  `id` int(30) NOT NULL,
+DROP TABLE IF EXISTS `service_list`;
+CREATE TABLE IF NOT EXISTS `service_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `category_ids` text NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `fee` float NOT NULL DEFAULT 0,
-  `delete_flag` tinyint(4) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `fee` float NOT NULL DEFAULT '0',
+  `delete_flag` tinyint NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `service_list`
+-- Déchargement des données de la table `service_list`
 --
 
 INSERT INTO `service_list` (`id`, `category_ids`, `name`, `description`, `fee`, `delete_flag`, `date_created`, `date_updated`) VALUES
@@ -136,25 +152,27 @@ INSERT INTO `service_list` (`id`, `category_ids`, `name`, `description`, `fee`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_info`
+-- Structure de la table `system_info`
 --
 
-CREATE TABLE `system_info` (
-  `id` int(30) NOT NULL,
+DROP TABLE IF EXISTS `system_info`;
+CREATE TABLE IF NOT EXISTS `system_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `meta_field` text NOT NULL,
-  `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `meta_value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `system_info`
+-- Déchargement des données de la table `system_info`
 --
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 (1, 'name', 'KALYX Veterinary Appointment System'),
 (6, 'short_name', 'KVAS'),
-(11, 'logo', 'uploads/logo-1641262650.png'),
+(11, 'logo', 'uploads/logo-1743330453.png'),
 (13, 'user_avatar', 'uploads/user_avatar.jpg'),
-(14, 'cover', 'uploads/cover-1641262651.png'),
+(14, 'cover', 'uploads/cover-1743330453.png'),
 (15, 'content', 'Array'),
 (16, 'email', 'info@vetclinic.com'),
 (17, 'contact', '0979822267 / 0842929457'),
@@ -167,119 +185,40 @@ INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(50) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `firstname` varchar(250) NOT NULL,
-  `middlename` text DEFAULT NULL,
+  `middlename` text,
   `lastname` varchar(250) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
-  `avatar` text DEFAULT NULL,
+  `avatar` text,
   `last_login` datetime DEFAULT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 0,
-  `status` int(1) NOT NULL DEFAULT 1 COMMENT '0=not verified, 1 = verified',
-  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `type` tinyint(1) NOT NULL DEFAULT '0',
+  `status` int NOT NULL DEFAULT '1' COMMENT '0=not verified, 1 = verified',
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `status`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1639468007', NULL, 1, 1, '2021-01-20 14:02:37', '2021-12-14 15:47:08'),
-(3, 'Claire', NULL, 'Blake', 'cblake', '4744ddea876b11dcb1d169fadf494418', 'uploads/avatar-3.png?v=1639467985', NULL, 2, 1, '2021-12-14 15:46:25', '2021-12-14 15:46:25');
+(1, 'BENOIT', NULL, 'BEYA', 'admin', 'd352dfee2f93b810dfe713208b776d16', 'uploads/avatar-1.png?v=1639468007', NULL, 1, 1, '2021-01-20 14:02:37', '2025-03-30 12:01:40'),
+(3, 'dina', NULL, 'kisuba', 'dkisuba', '827ccb0eea8a706c4c34a16891f84e7b', 'uploads/avatar-3.png?v=1639467985', NULL, 2, 1, '2021-12-14 15:46:25', '2025-03-31 09:18:13');
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `appointment_list`
---
-ALTER TABLE `appointment_list`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `category_list`
---
-ALTER TABLE `category_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `message_list`
---
-ALTER TABLE `message_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `service_list`
---
-ALTER TABLE `service_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `system_info`
---
-ALTER TABLE `system_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `appointment_list`
---
-ALTER TABLE `appointment_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `category_list`
---
-ALTER TABLE `category_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `message_list`
---
-ALTER TABLE `message_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `service_list`
---
-ALTER TABLE `service_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `system_info`
---
-ALTER TABLE `system_info`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `appointment_list`
+-- Contraintes pour la table `appointment_list`
 --
 ALTER TABLE `appointment_list`
   ADD CONSTRAINT `appointment_list_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category_list` (`id`) ON DELETE CASCADE;
